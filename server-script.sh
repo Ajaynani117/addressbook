d48ae69f7218b9e794b9758c7570babac0d9f84e
Ajaynani117 <sriramajay1173@gmail.com>
2025-04-07 19:20:16 -0500

    java11 is added

--- server-script.sh
+++ server-script.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-sudo yum  install java-1.8.0-openjdk-devel -y
+sudo yum install java-11-amazon-corretto-devel -y
 sudo yum install git -y
 sudo yum install maven -y
 if [ -d "addressbook" ]
